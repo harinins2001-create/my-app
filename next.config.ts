6c2c3ec6ae0c6010522a65b5630a24b59854de50
User <harinins2001@gmail.com>
2026-06-13 11:45:04 +0530

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // ⚠️ TypeScript errors තිබුණත් build එක ඉස්සරහටම යවන්න කියලා Next.js එකට කියනවා
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
